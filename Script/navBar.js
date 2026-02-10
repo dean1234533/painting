cf76c4d1cdcbcf36f42391ecd66122818f2f5161
dean1234533 <deanburt1308@gmail.com>
2026-02-10 12:47:11 +0000

    Add files via upload

---
+++ Script/navBar.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector(".navBar")
+const btn = document.querySelector(".Navbtn")
+
+function dropDown(){
+
+
+nav.classList.toggle("Active")
+
+
+
+
+}
+
+btn.addEventListener("click", ()=>{
+
+dropDown()
+
+})
